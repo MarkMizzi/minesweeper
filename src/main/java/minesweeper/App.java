11e23be212a7979d5b9b62ae604a92b838080447
MarkMizzi <mizzimark2001@gmail.com>
2022-10-26 06:12:56 +0200

    [task2] refine UI

--- src/main/java/minesweeper/App.java
+++ src/main/java/minesweeper/App.java
@@ -1,8 +1,12 @@
 package minesweeper;
 
-import javax.swing.*;
+import javax.swing.BorderFactory;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.border.Border;
-import java.awt.*;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.FlowLayout;
 
 public class App {
 
@@ -12,6 +16,8 @@ public class App {
     // gap between elements
     public static final int HGAP = 10, VGAP = 10;
     public static final int BORDER_THICKNESS = 10;
+
+    public static final String TITLE = "Minesweeper";
 
     public static final Color BG_COLOR = new Color(192, 192, 192);
 
@@ -26,22 +32,36 @@ public class App {
         this.boardViewController = new BoardViewController(new Board(16, 16, 50));
         this.newGameButton = new NewGameButton(this);
 
-        BorderLayout borderLayout = new BorderLayout();
-        borderLayout.setHgap(App.HGAP);
-        borderLayout.setVgap(App.VGAP);
+        // set up game bar
+
+        FlowLayout gameBarLayout = new FlowLayout(FlowLayout.CENTER);
+
+        JPanel gameBar = new JPanel();
+
+        gameBar.setLayout(gameBarLayout);
+
+        gameBar.add(this.newGameButton);
+
+        gameBar.setBackground(App.BG_COLOR);
+
+        // set up main content pane
+
+        BorderLayout contentLayout = new BorderLayout(App.HGAP, App.VGAP);
 
         Border border = BorderFactory.createLineBorder(App.BG_COLOR, App.BORDER_THICKNESS);
 
         JPanel content = new JPanel();
 
-        content.setLayout(borderLayout);
+        content.setLayout(contentLayout);
         content.add(boardViewController, BorderLayout.CENTER);
-        content.add(newGameButton, BorderLayout.NORTH);
+        content.add(gameBar, BorderLayout.NORTH);
 
         content.setBackground(App.BG_COLOR);
         content.setBorder(border);
 
-        JFrame window = new JFrame("Minesweeper");
+        // set up window
+
+        JFrame window = new JFrame(App.TITLE);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setContentPane(content);
 
--- src/main/java/minesweeper/NewGameButton.java
+++ src/main/java/minesweeper/NewGameButton.java
@@ -1,13 +1,47 @@
 package minesweeper;
 
+import javax.imageio.ImageIO;
 import java.awt.Button;
+import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Image;
 import java.awt.event.ActionEvent;
+import java.io.IOException;
+import java.net.URL;
 
 public class NewGameButton extends Button {
 
+    public static final int PREFERRED_WIDTH = 52, PREFERRED_HEIGHT = 52;
+
+    private static Image buttonImage;
+
+    // load image of smiley (buttonImage)
+    static {
+        String filename = "smiley.png";
+
+        try {
+            URL imageURL = ClassLoader.getSystemClassLoader().getResource(filename);
+
+            if (imageURL == null) {
+                System.out.printf("Couldn't find resource file %s", filename);
+                System.exit(-1);
+            }
+
+            buttonImage = ImageIO.read(imageURL);
+        } catch (
+                IOException e) {
+            System.out.printf("Error while reading resource file %s", filename);
+            System.exit(-1);
+        }
+    }
+
     NewGameButton(App app) {
-        super("New Game");
+        super();
+
+        this.setPreferredSize(new Dimension(
+                NewGameButton.PREFERRED_WIDTH,
+                NewGameButton.PREFERRED_HEIGHT
+        ));
 
         this.addActionListener(
                 (ActionEvent e) -> {
@@ -18,5 +52,6 @@ public class NewGameButton extends Button {
 
     public void paint(Graphics g) {
         super.paint(g);
+        g.drawImage(buttonImage, 0, 0, this.getWidth(), this.getHeight(), this);
     }
 }
