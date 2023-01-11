3555880652e758b7dc4a05d23f07e73a098cc1d3
MarkMizzi <mizzimark2001@gmail.com>
2023-01-11 10:03:16 +0100

    [task2] added more tests for BoardView

--- src/test/java/minesweeper/BoardViewTests.java
+++ src/test/java/minesweeper/BoardViewTests.java
@@ -9,7 +9,7 @@ import java.awt.*;
 public class BoardViewTests {
 
     @Test
-    public void testPreferredSizeFitsParent() {
+    public void testPreferredSizeFitsHorizontalParent() {
         BoardView boardView = new BoardView(
                 null,
                 new Board(16, 16, 40)
@@ -22,12 +22,73 @@ public class BoardViewTests {
 
         parent.revalidate();
 
-        int parentHeight = parent.getHeight(), parentWidth = parent.getWidth();
+        int parentHeight = parent.getHeight();
         Dimension preferredSize = boardView.getPreferredSize();
 
-        Assertions.assertEquals(
-                Math.min(parentHeight, parentWidth),
-                Math.min(preferredSize.height, preferredSize.width)
+        Assertions.assertEquals(parentHeight, preferredSize.height);
+    }
+
+    @Test
+    public void testPreferredSizeFitsVerticalParent() {
+        BoardView boardView = new BoardView(
+                null,
+                new Board(16, 16, 40)
         );
+
+        JPanel parent = new JPanel(new FlowLayout());
+        parent.setSize(768, 1024);
+
+        parent.add(boardView);
+
+        parent.revalidate();
+
+        int parentWidth = parent.getWidth();
+        Dimension preferredSize = boardView.getPreferredSize();
+
+        Assertions.assertEquals(parentWidth, preferredSize.height);
+    }
+
+    @Test
+    public void testAspectRatioScalingForVerticalBoard() {
+
+        Board board = new Board(16, 20, 40);
+
+        BoardView boardView = new BoardView(
+                null,
+                board
+        );
+
+        JPanel parent = new JPanel(new FlowLayout());
+        parent.setSize(1024, 768);
+
+        parent.add(boardView);
+
+        parent.revalidate();
+
+        Dimension preferredSize = boardView.getPreferredSize();
+
+        Assertions.assertEquals(preferredSize.height / board.rows(), preferredSize.width / board.cols());
+    }
+
+    @Test
+    public void testAspectRatioScalingForHorizontalBoard() {
+
+        Board board = new Board(20, 16, 40);
+
+        BoardView boardView = new BoardView(
+                null,
+                board
+        );
+
+        JPanel parent = new JPanel(new FlowLayout());
+        parent.setSize(1024, 768);
+
+        parent.add(boardView);
+
+        parent.revalidate();
+
+        Dimension preferredSize = boardView.getPreferredSize();
+
+        Assertions.assertEquals(preferredSize.height / board.rows(), preferredSize.width / board.cols());
     }
 }
